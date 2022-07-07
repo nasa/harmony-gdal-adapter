e03d4484893fd29f4b0779a8b739aa3ee4c8926c
Matt Savoie <savoie@colorado.edu>
2022-07-07 17:43:06 -0600

    Adds first unit tests for add_to_list
    Just describes expected behavior
    
    DAS-1479

--- tests/unit/test_transform.py
+++ tests/unit/test_transform.py
@@ -1,8 +1,15 @@
+from string import ascii_letters, digits
+from random import choice
 from unittest import TestCase
-from unittest.mock import patch, ANY
+from unittest.mock import patch, ANY, Mock
 from gdal_subsetter.transform import HarmonyAdapter
+from gdal_subsetter.exceptions import UnstackableVariablesError
 
 from harmony.message import Message
+
+
+def random_file(size=6, chars=ascii_letters + digits):
+    return ''.join(choice(chars) for _ in range(size))
 
 
 class TestRecolor(TestCase):
@@ -19,7 +26,7 @@ class TestRecolor(TestCase):
         })
         test_adapter = HarmonyAdapter(test_message, '', None)
 
-        src_file = 'sourcefile';
+        src_file = 'sourcefile'
         with patch.object(test_adapter, 'cmd') as mock_cmd:
             result = test_adapter.recolor('granId__subsetted', src_file,
                                           'dstdir')
@@ -44,8 +51,7 @@ class TestRecolor(TestCase):
         expected_outfile = f'{dest_dir}/{layer_id}__colored.png'
 
         with patch.object(test_adapter, 'cmd') as cmd_mock:
-            result = test_adapter.recolor(layer_id, 'srcfile',
-                                          dest_dir)
+            result = test_adapter.recolor(layer_id, 'srcfile', dest_dir)
             self.assertEqual(result, expected_outfile,
                              'incorrect output file generated')
             cmd_mock.assert_called_once_with('gdaldem', 'color-relief',
@@ -56,4 +62,61 @@ class TestRecolor(TestCase):
             copyfile_mock.assert_any_call(expected_outfile,
                                           f'{dest_dir}/result.png')
             copyfile_mock.assert_any_call(
-                expected_outfile.replace('.png', '.wld'), f'{dest_dir}/result.wld')
+                expected_outfile.replace('.png', '.wld'),
+                f'{dest_dir}/result.wld')
+
+
+class TestAddToList(TestCase):
+
+    def test_add_to_result_stacks_if_stackable_with_netcdf(self):
+
+        filelist = [random_file(), random_file()]
+        dstdir = random_file()
+        dstfile = f'{dstdir}/result.tif'
+
+        test_adapter = HarmonyAdapter(
+            Message({"format": {
+                "mime": "application/x-netcdf4"
+            }}), '', None)
+
+        stack_mock = Mock()
+        stack_mock.return_value = dstfile
+
+        checkstackable_mock = Mock()
+        checkstackable_mock.return_value = True
+
+        test_adapter.stack_multi_file_with_metadata = stack_mock
+        test_adapter.checkstackable = checkstackable_mock
+
+        result = test_adapter.add_to_result(filelist, dstdir)
+
+        stack_mock.assert_called_once_with(filelist, dstfile)
+        self.assertEqual(result, dstfile)
+
+    def test_add_to_result_raises_if_unstackable_with_netcdf(self):
+
+        filelist = [random_file(), random_file()]
+        dstdir = random_file()
+        dstfile = f'{dstdir}/result.tif'
+
+        test_adapter = HarmonyAdapter(
+            Message({"format": {
+                "mime": "application/x-netcdf4"
+            }}), '', None)
+
+        stack_mock = Mock()
+        stack_mock.return_value = dstfile
+
+        checkstackable_mock = Mock()
+        checkstackable_mock.return_value = False
+
+        test_adapter.stack_multi_file_with_metadata = stack_mock
+        test_adapter.checkstackable = checkstackable_mock
+
+        with self.assertRaises(UnstackableVariablesError) as error:
+            test_adapter.add_to_result(filelist, dstdir)
+
+        stack_mock.assert_not_called()
+        self.assertEqual(
+            str(error.exception),
+            'Request cannot be completed: the datasets cannot be stacked.')
